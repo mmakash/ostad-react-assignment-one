b1f37845f8bfa6d6e8d75c41825f6999a7d505e6
mmakash <musaddekurakash@gmail.com>
2023-10-21 22:37:05 +0600

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Header = () => {
+    const items = ["Home","About","Project","Service","Contact"];
     return (
         <div>
             <h1>Hello From Header</h1>
+            <ul>
+                {
+                    items.map((item) => {
+                        return (
+                            <li key={item}>{item}</li>
+                        );
+                    })
+                }
+            </ul>
         </div>
     );
 };
